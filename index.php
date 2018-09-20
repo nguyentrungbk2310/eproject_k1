692c9bb4f845b9699994d224005d09c6b0adb320
hoang ky anh <kyanh96vn@gmail.com>
2018-09-20 21:44:33 +0700

    add new file index.php length.php mysite.css

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	 <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="mysite.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+</head>
+<body>
+	<nav class="navbar navbar-fixed-top navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle = "collapse" data-target = "#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Logo</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Sitemap</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span>Admin</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container conversion-chart">
+		<h2 class="text-center text-primary">Metric conversion charts</h2>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading text-center" >
+						<a href="#" style="color: white;"><h3>Area</h3></a>
+					</div>
+					<div class="panel-body">
+						<a href="#"><div>Square inches Square millimeters</div></a>
+						<a href="#"><div>Square feet Square meters</div></a>
+						<a href="#"><div>Acres Hectares</div></a>
+						<a href="#"><div>Square yards Square meters</div></a>
+						<a href="#" class="text-center text-danger"><p>Read more</p></a>
+					</div>	
+				</div>
+			</div>
+			<div class="col-sm-4">	
+				<div class="panel panel-primary">
+					<div class="panel-heading text-center">
+						<a href="#" style="color: white;"><h3>Length</h3></a>
+					</div>
+					<div class="panel-body">
+					<a href="#"><div>Feet Meters</div></a>
+					<a href="#"><div>Yards - Meters</div></a>
+					<a href="#"><div>Miles Kilometers</div></a>
+					<a href="#"><div>xxx</div></a>
+					<a href="#" class="text-center text-warning"><p>Read more</p></a>
+					</div>
+				</div>						
+			</div>
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading text-center">
+						<a href="#" style="color: white;"><h3>Volume</h3></a>
+					</div>
+					<div class="panel-body">
+						<a href="#"><div>Fluid ounces milliliters</div></a>
+						<a href="#"><div>Gallons liters</div></a>
+						<a href="#"><div>Cubic feet - Cubic meters</div></a>
+						<a href="#"><div>Cubic yards - Cubic meters</div></a>
+						<a href="#" class="text-center text-warning"><p>Read more</p></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading text-center">
+						<a href="#" style="color: white;"><h3>Mass</h3></a>
+					</div>
+					<div class="panel-body">
+						<a href="#"><div>Ounces grams</div></a>
+						<a href="#"><div>Pounds kilograms</div></a>
+						<a href="#"><div>Short tons - mega grams</div></a>
+						<a href="#"><div>xxx</div></a>
+						<a href="#" class="text-center text-warning"><p>Read more</p></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading text-center">
+						<a href="#" style="color: white"><h3>Temperature</h3></a>
+					</div>
+					<div class="panel-body">
+						<a href="#"><div>Fahrenheit Celsius</div></a>
+						<a href="#"><div>Celsius Fahrenheit</div></a>
+						<a href="#"><div>xxx</div></a>
+						<a href="#"><div>xxx</div></a>
+						<a href="#" class="text-center text-warning"><p>Read more</p></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading text-center">
+						<a href="#" style="color: white"><h3>Currency</h3></a>
+					</div>
+					<div class="panel-body">
+						<a href="#"><div>Euro</div></a>
+						<a href="#"><div>United States Dollar</div></a>
+						<a href="#"><div>Japanese Yen</div></a>
+						<a href="#"><div>Chineses Yuan</div></a>
+						<a href="#" class="text-center text-warning"><p>Read more</p></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="container">
+			<div class="text-center">
+				<h3>WEBSITE NAME - WEBSITE SHARING THE KNOWLEDGE OF THE SYSTEM OF MEASUREMENT AND HELP YOU CONVERTED METRIC<h3>
+			</div>
+			<p>
+				The metric system originated in France in 1799 following the French Revolution although decimal units had been used in many other countries and cultures previously. Although there have been many different measurements and the definitions of the units have been revised, the official system of measurements of most countries is the modern form of the metric system which is known as the "International System of Units".
+			</p>
+			<p>
+				Since other systems of measurement are still used around the world, such as the United States and the United Kingdom, this site aims to help people convert units of measurement with Metric Converter and Metric Conversion Table and to better understand alternative measurements that they are unfamiliar with. The measurement units are categorized into types (such as Temperature Conversion, Weight Conversion and so on) seen on the right-hand side which then lead to a series of metric conversion calculators.
+			</p>	
+		</div>
+	</div>
+	<div class="container">
+		<div id="myCarousel" class="carousel slie" data-ride = "carousel">
+			<ol class="carousel-indicators">
+		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#myCarousel" data-slide-to="1"></li>
+		    <li data-target="#myCarousel" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		  	<div class="item active">
+		  		<img src="https://i.9mobi.vn/cf/images/2015/03/nkk/nhung-hinh-anh-dep-20.jpg" style="width: 100%;">
+		  		<div class="carousel-caption">
+		  				<a href="#"><h3>Articles</h3></a>
+				  		<a href="#"><p>The SI System </p></a>
+				  		<a href="#"><p>Metric System </p></a>
+				  		<a href="#"><p>Traditional U.S. Units</p></a>
+		  		</div>
+		  	</div>
+		  	<div class="item">
+		  		<img src="https://i.9mobi.vn/cf/images/2015/03/nkk/nhung-hinh-anh-dep-20.jpg" style="width: 100%;">
+		  		<div class="carousel-caption">
+		  			<a href="#"><h3>FAQs</h3></a>
+		  			<a href="#"><p>How do I enter numbers in Scientific Notation?</p></a>
+		  			<a href="#"><p>What is a knot? What is a nautical mile?</p></a>
+		  			<a href="#"><p>What about rainfall? How do I convert between inches of rain, and millimeters of rain?</p></a>
+		  		</div>
+		  	</div>
+		  	<div class="item">
+		  		<img src="https://i.9mobi.vn/cf/images/2015/03/nkk/nhung-hinh-anh-dep-20.jpg" style="width: 100%;">
+		  		<div class="carousel-caption">
+		  			<a href="#"><h3>Conversion</h3></a>
+		  			<a href="#"><p>Area</p></a>
+		  			<a href="#"><p>Length</p></a>
+		  			<a href="#"><p>Volume</p></a>
+		  			<a href="#"><p>Mass</p></a>
+		  			<a href="#"><p>Temperature</p></a>
+		  			<a href="#"><p>Currency</p></a>
+		  		</div>
+		  	</div>
+		  </div>
+		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+	</div>
+	<div class="container-fluid footer" style="background: #54575b;">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<h2>About us</h2>
+					<p>We create this website aims to help people convert units of measurement with Metric Converter and Metric Conversion Table and to better understand alternative measurements that they are unfamiliar with. The measurement units are categorized into types (such as Temperature Conversion, Weight Conversion and so on) seen on the right-hand side which then lead to a series of metric conversion calculators.</p>
+					<p><span class="glyphicon glyphicon-map-marker"></span> 285, Doi Can, Ba Dinh, Ha Noi</p>
+					<p><span class="glyphicon glyphicon-phone"></span> 0123456789</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> aprotrainaptech@gmail.com</p>
+				</div>
+				<div class="col-sm-2">
+					<h2>Associate</h2>
+					<a href="#"><div>Home</div></a>
+					<a href="#"><div>Contact</div></a>
+				</div>
+				<div class="col-sm-2">
+					<h2>Policy</h2>
+					<a href="#"><p>Introduce</p></a>
+					<a href="#"><p>Copyright</p></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid" style="background: black;">
+		<h4 class="container">&copy Copyright - Website Name All Rights Reserved</h4>
+	</div>
+</body>
+</html>
---
+++ length.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="mysite.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-fixed-top navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle = "collapse" data-target = "#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Logo</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Sitemap</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span>Admin</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<h2 class="text-center text-primary content">Length Conversion</h2>\
+	<div class="container">
+		form chuyen doi
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<ul class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#">Area</a></li>
+					<li class="active"><a href="#">Length</a></li>
+					<li><a href="#">Feet Meters</a></li>
+					<li><a href="#">Yards - Meters</a></li>
+					<li><a href="#">Miles Kilometers</a></li>
+					<li class="active"><a href="#">Volume</a></li>
+					<li class="active"><a href="#">Mass</a></li>
+					<li class="active"><a href="#">Temperature</a></li>
+					<li class="active"><a href="#">Currency</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-9">
+				Noi dung
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid" style="background: #54575b;">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<h2>About us</h2>
+					<p>We create this website aims to help people convert units of measurement with Metric Converter and Metric Conversion Table and to better understand alternative measurements that they are unfamiliar with. The measurement units are categorized into types (such as Temperature Conversion, Weight Conversion and so on) seen on the right-hand side which then lead to a series of metric conversion calculators.</p>
+					<p><span class="glyphicon glyphicon-map-marker"></span> 285, Doi Can, Ba Dinh, Ha Noi</p>
+					<p><span class="glyphicon glyphicon-phone"></span>0123456789</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> aprotrainaptech@gmail.com</p>
+				</div>
+				<div class="col-sm-2">
+					<h2>Associate</h2>
+					<a href="#"><div>Home</div></a>
+					<a href="#"><div>Contact</div></a>
+				</div>
+				<div class="col-sm-2">
+					<h2>Policy</h2>
+					<a href="#"><p>Introduce</p></a>
+					<a href="#"><p>Copyright</p></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid" style="background: black;">
+		<h4 class="container">&copy Copyright - Website Name All Rights Reserved</h4>
+	</div>
+</body>
+</html>
